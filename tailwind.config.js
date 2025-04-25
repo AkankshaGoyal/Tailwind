fa5e88197d7e26c416c615965695f1d1c84899b4
Akanksha Goyal <akankshagoyalju2000@gmail.com>
2025-04-25 09:03:23 +0530

    Hands on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
   content: ["./dist/*.html"],
  
   theme: {
+    screens:{
+          sm:"340px",
+          md:"540px",
+          lg:"768px",
+          xl:"1180px"
+    },
     extend: {},
+    container:{
+      center:true,
+      padding:{
+        DEFAULT:"12px",
+        md:"32px"
+      }
+    }
   },
   plugins: [],
 }
